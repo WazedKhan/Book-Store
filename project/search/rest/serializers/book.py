9f8d1ab19f02c2f39b1d60c7c4e806f838215345
WazedKhan <wazedkhan119399@gmail.com>
2023-05-25 00:51:58 +0600

    change filtering product by stock

--- project/search/rest/serializers/book.py
+++ project/search/rest/serializers/book.py
@@ -4,7 +4,7 @@ from author.rest.serializer.author import SlimAuthorSerializer
 
 from publisher.rest.serializers.publisher import SlimPublisherSerializer
 
-from book.models import Book
+from book.models import Book, Stock
 
 
 class SearchBooksSerializer(ModelSerializer):
@@ -14,3 +14,11 @@ class SearchBooksSerializer(ModelSerializer):
     class Meta:
         model = Book
         fields = ["id", "name", "pages", "price", "author", "publisher"]
+
+
+class SearchStockSerializer(ModelSerializer):
+    book = SearchBooksSerializer()
+
+    class Meta:
+        model = Stock
+        fields = ["id", "quantity", "in_stock", "last_updated", "location", "book"]
--- project/search/rest/views/book.py
+++ project/search/rest/views/book.py
@@ -4,25 +4,25 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 
 from ..serializers import book
-from book.models import Book
+from book.models import Stock
 
 
 class SearchBooksList(ListAPIView):
     """search's book with related fields"""
 
-    queryset = Book.objects.all()
-    serializer_class = book.SearchBooksSerializer
+    queryset = Stock.objects.all()
+    serializer_class = book.SearchStockSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["name"]
+    filterset_fields = ["id"]
 
     def get_queryset(self):
         keyword = self.request.query_params.get("keyword", None)
         queryset = super().get_queryset()
         if keyword:
             queryset = queryset.filter(
-                Q(author__name__icontains=keyword.lower())
-                | Q(name__icontains=keyword.lower())
-                | Q(publisher__name__icontains=keyword.lower())
-            ).order_by("author__name")
+                Q(book__author__name__icontains=keyword.lower())
+                | Q(book__name__icontains=keyword.lower())
+                | Q(book__publisher__name__icontains=keyword.lower())
+            ).order_by("book__author__name")
 
         return queryset
