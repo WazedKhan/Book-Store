e0631085f775bd6abd72e9e75792f1a4dcf3791c
Abdul Wajed Khan <51821594+WazedKhan@users.noreply.github.com>
2023-06-24 06:13:18 +0000

    removed / from beginning

--- project/search/rest/urls/__init__.py
+++ project/search/rest/urls/__init__.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 app_name = "book"
 
 urlpatterns = [
-    path("/book", include("search.rest.urls.book")),
+    path("book", include("search.rest.urls.book")),
 ]
